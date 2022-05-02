4b63d2bf531c6ab5ffd57df1344976e014bf97d4
Omdev-Paul <101262334+Omdev-Paul@users.noreply.github.com>
2022-05-02 09:40:13 +0530

    Create perfect_number.c

---
+++ perfect_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, Sum = 0;
+    printf("\n Please Enter any number \n");
+    scanf("%d", &n);
+
+    for (i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+            Sum += i;
+    }
+
+    if (Sum == n)
+        printf("\n %d is a Perfect Number", n);
+    else
+        printf("\n%d is not the Perfect Number", n);
+
+    return 0;
+}
